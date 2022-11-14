308fb64ad6747e32f0973209eea101a57fac447c
armadapenantian <118154754+armadapenantian@users.noreply.github.com>
2022-11-14 05:58:51 +0000

    Deploying to gh-pages from @ armadapenantian/website@3c999bb6de79ef3dc1271c6a2d0972721c047e35 🚀

---
+++ _next/static/CgJ_BQheSqZXeywT5XzML/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/594-fbb929344c5e7c28.js","static/chunks/pages/index-16d87b3c4ce6e569.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
