3c999bb6de79ef3dc1271c6a2d0972721c047e35
Patricia <patricia@prixa.ai>
2022-11-14 12:57:53 +0700

    Add image unoptimized

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    newNextLinkBehavior: true,
-    scrollRestoration: true,
-    images: {
-      allowFutureImage: true,
-      unoptimized: true,
-    },
+module.exports = {
+  images: {
+    unoptimized: true,
   },
 };
-
-module.exports = nextConfig;
